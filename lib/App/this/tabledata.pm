681ba1a193be6144f9bbb7f12ff4c794cd7c0672
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-30 13:37:41 +0700

    Initial commit

---
+++ lib/App/this/tabledata.pm
@@ -0,0 +1,22 @@
+package App::this::tabledata;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Return name of TableData:: Perl module (without the prefix) associated with the current directory
+
+=head1 DESCRIPTION
+
+See included scripts:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::TableDataUtils>
